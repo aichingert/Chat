fe93103376b4f353a828e5db60a749c5fffbb5f9
VeQox <sonnleitner.luki@gmail.com>
2023-03-01 12:16:13 +0100

    add headers to login @aichingert usefull commit ik

--- chat-frontend/src/routes/auth/login/+page.server.ts
+++ chat-frontend/src/routes/auth/login/+page.server.ts
@@ -16,6 +16,9 @@ export const actions : Actions = {
         try{
             let raw = await fetch("http://127.0.0.1:3000/login", {
                 method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
                 body: JSON.stringify({
                     name: username,
                     password: password
